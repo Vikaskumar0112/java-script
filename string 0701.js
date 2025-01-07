598325ca85e18b40a89db68ea7f5ea5bf2c661b3
Vikaskumar0112 <vikaskumar011296@gmail.com>
2025-01-07 19:49:27 +0530

    commit

---
+++ string 0701.js
@@ -0,0 +1,30 @@
+//string  -> code c
+
+let str = "Hello"
+console.log(str[1]);
+
+console.log(str.toUpperCase())
+
+console.log(str.toLowerCase())
+
+let ct =str.repeat(3)
+
+console.log(ct);
+
+// template  literals  '  '
+
+let tr="Trainer"
+let str1 =`Hello 
+my name is vikas  
+how are you
+i am Technocal ${tr}`
+
+console.log(str1);
+
+let fname = "vikas"
+let lname = "kumar"
+
+let name = fname.concat(lname)
+
+
+console.log(name);
