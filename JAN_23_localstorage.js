ac7f5a0d481969f5c418a9b91b887e508463e2b6
Vikaskumar0112 <vikaskumar011296@gmail.com>
2025-01-24 20:00:09 +0530

    success

--- JAN_23_localstorage.js
+++ JAN_23_localstorage.js
@@ -1,11 +1,35 @@
-localStorage.setItem("data","prince")
-let name="mobile"
-localStorage.setItem("mblname",name)
-localStorage.setItem("mblname","ok")
+// localStorage.setItem("data","prince")
+// let name="mobile"
+// localStorage.setItem("mblname",name)
+// localStorage.setItem("mblnam","ok")
+// localStorage.setItem("mblnamO","ok1")
 
-let nm = localStorage.getItem("mblname")
-document.write("<h1>"+nm+"</h1")
+// let nm = localStorage.getItem("mblname")
+// document.write("<h1>"+nm+"</h1>")
 
-localStorage.removeItem("mblname")
+// // localStorage.removeItem("mblname")
 
-localStorage.clear()
+// // localStorage.clear()
+
+
+
+// let person_ob= {
+//     name : "vikas",
+//     email : "vikas123@gmail.com",
+//     password : "123vd"
+// }
+
+// localStorage.setItem("userdata",JSON.stringify(person_ob))
+
+// let data=localStorage.getItem("userdata")
+// console.log(data)
+
+
+const datasubmit =()=>{
+    let userdata = {
+        name : document.querySelector('#uname').value,
+        name : document.querySelector('#uemail').value,
+        name : document.querySelector('#upass').value,
+    }
+    localStorage.setItem("userdata",JSON.stringify(userdata))
+}
